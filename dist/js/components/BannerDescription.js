5e5697ae42d5dc592b2a24485a873c174114ec9f
Hazel Garro <hazelvaleriagarroarias@gmail.com>
2023-04-28 00:07:30 -0600

    correccion de margen y tamanho letra en banner de index, se empezo el responsive

--- dist/js/components/BannerDescription.js
+++ dist/js/components/BannerDescription.js
@@ -12,9 +12,9 @@ app.component('banner-description',{
     template: 
     /* html */
     `<div class="row">
-    <div class="col-lg-5  col-md-12">
+    <div class="col-lg-7  col-md-12">
         <h1 class="mt-5">{{title}}</h1>
-        <p class="mt-3">{{description}}</p>
+        <p class="mt-3 text-description-banner">{{description}}</p>
     </div>
     <div class="col-4 ocultar">
         <p><img src="images/imgs/Maskgroup.png" alt="photo food"></p>
