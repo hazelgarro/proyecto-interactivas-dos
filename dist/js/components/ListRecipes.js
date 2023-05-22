995d6299bab2ad7f3c399099a5de6c095ba92a4b
Hazel Garro <hazelvaleriagarroarias@gmail.com>
2023-05-21 21:10:39 -0600

    recetas apartir del API en top recipes, profile user y recipes list admin, falta direccionar al detalle de la reseta, no sirve ni del lado del usuario ni del admin

--- dist/js/components/ListRecipes.js
+++ dist/js/components/ListRecipes.js
@@ -1,15 +1,24 @@
 //Componente de lista de recetas para el administrador
 app.component('list-recipes', {
+    props: {
+        title: {
+            type: String,
+            default: "default title"
+        },
+        image: {
+            type: String,
+        },
+    },
     template:
         /* html */
         `<div class="">
             <div class="row ms-5">
                 <div class="col-3">
-                    <img class="size-img-list" src="images/images-recipes/recipe-1exZxh80HP.png">
+                    <img class="size-img-list" v-bind:src="image">
                 </div> 
                 <div class="col-7 center center-items-list">
                     <div class="row position-text-list-recipes">
-                        <h3>Recipe Name</h3>
+                        <h3>{{title}}</h3>
                         <p>Update 3min ago</p>
                     </div>
                 </div>
