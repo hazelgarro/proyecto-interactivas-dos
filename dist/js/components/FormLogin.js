2e4c26684ea4073f8c2a9c46b21bb62ac996f91f
Hazel Garro <hazelvaleriagarroarias@gmail.com>
2023-07-02 12:20:57 -0600

    login

--- dist/js/components/FormLogin.js
+++ dist/js/components/FormLogin.js
@@ -1,23 +1,48 @@
 //Componente del formulario para ingresar
 app.component('form-login', {
+    methods: {
+        login() {
+          const user = document.getElementById('user').value;
+          const password = document.getElementById('password').value;
+    
+          // Realizar la solicitud de inicio de sesión al API
+          axios.post('http://localhost/prueba/public/api/users/login', {
+            email: user,
+            password: password
+          })
+          .then(response => {
+            // Solicitud de inicio de sesión exitosa
+            // Redirigir al usuario a ../../home.html
+            window.location.href = 'http://localhost/TheKitchen2.0/dist/index.html';
+          })
+          .catch(error => {
+            // Error en la solicitud de inicio de sesión
+            console.log(error);
+            // Puedes mostrar un mensaje de error o tomar otras acciones según tus necesidades
+          });
+        }
+      },
+
     template:
         /* html */
         `<div class="row container center mt-1 col-md-12 margin-box box-width">
     <div class="row box-login col-md-12 center ">
 
         <form action="login.php" method="post">
-            <label class="form-label text-forms" for="username">User</label>
-            <input id="username" class="form-control input-color" type="text" name="username">
+            <label class="form-label text-forms" for="user">Email</label>
+            <input id="user" class="form-control input-color" type="text" name="user">
             <div class="password-icon">
                 <label for="password" class="form-label mt-4 text-forms">Password <span class="text-question"><a
-                            href="" class="text-question"> Forgot?</a></span></label>
+                            href="./pass-recovery-one.html" class="text-question"> Forgot?</a></span></label>
                 <input id="password" name="password" type="password" class="form-control input-color">
                 <img class="input-icon-position-login" src="./images/imgs/icons/visibility.svg">
             </div>
 
             <div class="col center-button">
                 <div class="row center">
-                    <button class="center btn-orange-dark mt-3 hover-grow"><a class="header-link" href="index.html" style="text-decoration: none;">Log in</a></button>
+                    <!-- Agregar el evento 'click' para llamar al método 'loginUser' -->
+                    <a href="#" class="center btn-orange-dark mt-3 hover-grow" @click="login">Log in</a>
+                    <!--<button class="center btn-orange-dark mt-3 hover-grow"><a class="header-link" href="index.html" style="text-decoration: none;">Log in</a></button>-->
                 </div>
                 <div class="">
                     <span class="line-deco mt-3 mb-3 mx-auto"></span>
--- dist/js/components/FormSignUp.js
+++ dist/js/components/FormSignUp.js
@@ -1,5 +1,39 @@
 //Formulario para el registro
 app.component('form-sign-up', {
+    methods: {
+        registerUser() {
+            // Obtener los valores de los campos de entrada
+            const name = document.getElementById('name').value;
+            const lastName = document.getElementById('lastname').value;
+            const country = document.getElementById('country').value;
+            const email = document.getElementById('email').value;
+            //const user = document.getElementById('user').value;
+            const password = document.getElementById('password').value;
+
+            // Crear el objeto de datos a enviar en la solicitud POST
+            const data = {
+                name: name,
+                last_name: lastName,
+                country: country,
+                email: email,
+                //user: user,
+                password: password,
+            };
+
+            // Realizar la solicitud POST al endpoint de registro
+            axios.post('http://localhost/prueba/public/api/users/register', data)
+                .then(response => {
+                    // Registro exitoso
+                    console.log(response.data);
+                    // Redirigir a ../../login
+                    window.location.href = '../../login.html';
+                })
+                .catch(error => {
+                    // Error en el registro
+                    console.error(error);
+                });
+        }
+    },
     template:
         /* html */
         `<div class="row container center mt-1 col-md-12 margin-box box-width">
@@ -10,28 +44,30 @@ app.component('form-sign-up', {
             <input id="name" class="form-control input-color" type="text" name="name">
             <label class="form-label text-forms mt-2" for="lastname">Last Name</label>
             <input id="lastname" class="form-control input-color" type="text" name="lastname">
+            <label class="form-label" for="country">Country</label>
+            <input id="country" class="form-control mb-2" type="text" name="country">
             <label class="form-label text-forms mt-2" for="email">Email</label>
             <input id="email" class="form-control input-color" type="mail" name="email">
-            <label class="form-label text-forms mt-2" for="user">User</label>
-            <input id="user" class="form-control input-color" type="text" name="user">
+            <!--<label class="form-label text-forms mt-2" for="user">User</label>
+            <input id="user" class="form-control input-color" type="text" name="user">-->
             <div class="password-icon">
                 <label for="password" class="form-label mt-2 text-forms">Password</label>
                 <input name="password" type="password" class="form-control input-color">
                 <img class="input-icon-position-signup" src="./images/imgs/icons/visibility.svg">
             </div>
 
-
-            
             <div class="col center-button">
                 <div class="row center">
-                    <button class="center btn-orange-light mt-4 hover-grow" type="submit"><a class="header-link" href="login.html" style="text-decoration: none; color:black">Sign Up</a></button>
+                    <a href="./login.html" class="center btn-orange-light mt-4 hover-grow header-link" @click="registerUser" style="text-decoration: none; color:black">Sign up</a>
+                    <!--<button class="center btn-orange-light mt-4 hover-grow" type="submit"><a class="header-link" href="login.html" style="text-decoration: none; color:black">Sign Up</a></button>-->
                 </div>
                 <div class="">
                     <span class=" line-deco mt-3 mb-3 mx-auto"></span>
                 </div>
                 <div class="row center">
-                    <button class="center btn-orange-dark mt-1 hover-grow" type="submit"
-                        value="Log in"><a class="header-link" href="login.html" style="text-decoration: none; color:black">Log in</a></button>
+                    <a href="./login.html" class="center btn-orange-dark mt-1 hover-grow header-link" style="text-decoration: none; color:black">Log in</a>
+                    <!--<button class="center btn-orange-dark mt-1 hover-grow" type="submit"
+                        value="Log in"><a class="header-link" href="login.html" style="text-decoration: none; color:black">Log in</a></button>-->
                 </div>
         </form>
     </div>
