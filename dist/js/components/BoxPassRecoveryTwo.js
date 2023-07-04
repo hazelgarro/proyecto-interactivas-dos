924296f09e4db5886828a9b9f3871372a59b2cb9
Hazel Garro <hazelvaleriagarroarias@gmail.com>
2023-07-03 19:51:45 -0600

    recovery password funcional

--- dist/js/components/BoxPassRecoveryTwo.js
+++ dist/js/components/BoxPassRecoveryTwo.js
@@ -1,16 +1,27 @@
 //Componente del cuadro que muestra el texto del Pass Recovery Two
 app.component('box-pass-recovery-two', {
+    data() {
+        return {
+            password: localStorage.getItem("newPassword"),
+            message: localStorage.getItem("message"),
+        }
+    },
     template:
         /* html */
         `<div class="container center mt-1 col-md-12 margin-box box-width">
         <div class="row box-pass-small center mt-2 col margin-box center-img-detail">
             <div>
                 <i class="center mt-3"><img src="images/imgs/icons/task_alt.svg" alt="task icon"></i>
-                <p class="text-center mt-4 text-med">Email Sent</p>
-                <p class="text-center mt-2">Check your email and open the link we sent to continue</p>
+                <p class="text-center mt-4">{{message}}</p>
+                <p class="text-center mt-2 text-med">New password: {{password}}</p>
+                <div class="row center margin-box">
+                    <a class="center btn-orange-dark mt-4 hover-grow" href="./login.html">Back to Login</a>
+                </div>
             </div>
         </div>
     </div>`
 })
 
+localStorage.setItem("newPassword", response.data.password);
+              localStorage.setItem("message", response.data.message);
 
--- dist/js/components/FormPassRecoveryOne.js
+++ dist/js/components/FormPassRecoveryOne.js
@@ -1,5 +1,28 @@
 //Componente, formulario del Pass Recovery One, para ingresar el correo al que se le hara llegar el link para cambio de contraseña
 app.component('form-pass-recovery-one', {
+    methods: {
+        recoveryPassword() {
+          const email = document.getElementById('email').value;
+    
+          // Realizar la solicitud de pass recovery al API
+          axios.post('http://localhost/prueba/public/api/users/recoverpassword', {
+            email: email
+          })
+            .then(response => {
+
+              localStorage.setItem("newPassword", response.data.password);
+              localStorage.setItem("message", response.data.message);
+    
+              window.location.href = 'http://localhost/TheKitchen2.0/dist/pass-recovery-two.html';
+    
+            })
+            .catch(error => {
+              // Error en la solicitud de inicio de sesión
+              console.log(error);
+              // Puedes mostrar un mensaje de error o tomar otras acciones según tus necesidades
+            });
+        }
+      },
     template:
         /* html */
         `<div class="row container center mt-1 col-md-12 margin-box box-width">
@@ -11,8 +34,7 @@ app.component('form-pass-recovery-one', {
                         <input id="email" class="form-control input-color" type="email" placeholder="Enter your email" name="email">
                         <div class="col col-md-12 center-button margin-box ">
                             <div class="row center margin-box">
-                                <a href="#" class="center btn-orange-dark mt-4 hover-grow">Send link to
-                                    email</a>
+                                <a class="center btn-orange-dark mt-4 hover-grow" @click="recoveryPassword">Send email</a>
                             </div>
                         </div>
                     </form>
